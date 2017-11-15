15f734083fa77c746275e0e47308413bd34f4db3
jesusVMayor <jesus@comunitea.com>
2017-11-15 18:29:12 +0100

    Removed Yahoo service.

--- currency_rate_update/services/__init__.py
+++ currency_rate_update/services/__init__.py
@@ -2,7 +2,6 @@
 from . import update_service_CA_BOC
 from . import update_service_CH_ADMIN
 from . import update_service_ECB
-from . import update_service_YAHOO
 from . import update_service_PL_NBP
 from . import update_service_MX_BdM
 from . import update_service_RO_BNR
